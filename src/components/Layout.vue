e3ea8f01dd7f3821843ab319c5fea9661daa3a77
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2021-04-09 13:50:06 +0200

    adjusted styles in component Layout

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="wrapper">
-    <div class="container">
+    <div class="container column">
       <label
-        >Wybierz plik
+        >Dodawanie plików do katalogu
         <input
+          class="file-input column"
           type="file"
           id="file"
           name="file"
@@ -12,12 +13,13 @@
         />
       </label>
     </div>
-
-    <br />
     <div class="container">
-      <button v-on:click="submitFiles()">Zapisz</button>
+      <h2 class="message">{{ message }}</h2>
+    </div>
+    <div class="container">
+      <button class="btn" v-on:click="submitFiles()">Zapisz</button>
 
-      <button v-on:click="handleDisplayFiles()">
+      <button class="btn" v-on:click="handleDisplayFiles()">
         Wczytaj pliki z serwera
       </button>
     </div>
@@ -68,21 +70,25 @@ export default {
     async submitFiles() {
       const formData = new FormData();
       formData.append('file', this.file);
-      try {
-        await axios.post('/api/upload', formData, {
-          headers: { 'Content-Type': 'multipart/form-data' },
-        });
-        this.message = 'Plik zapisany!';
-        this.handleDisplayFiles();
-      } catch (err) {
-        console.log('Error');
-        this.message = 'Coś poszło nie tak, spróbuj ponownie!';
+      if (this.files.some((image) => image.name === this.file.name)) {
+        this.message = 'Plik o takiej nazwie już istnieje!';
+      } else {
+        try {
+          await axios.post('/api/upload', formData, {
+            headers: { 'Content-Type': 'multipart/form-data' },
+          });
+          this.handleDisplayFiles();
+          this.message = 'Plik zapisany!';
+        } catch (err) {
+          console.log('Error');
+          this.message = 'Coś poszło nie tak, spróbuj ponownie!';
+        }
       }
     },
     handleFileUpload() {
       const file = this.$refs.file.files[0];
       this.file = file;
-      this.message = '';
+      this.message = file.name;
     },
     handleDisplayFiles() {
       try {
@@ -100,8 +106,8 @@ export default {
         axios.get(`/api/delete/${name}`).then((res) => {
           console.log(res.data);
         });
-        this.message = 'Plik skaowany!';
         this.handleDisplayFiles();
+        this.message = 'Plik skaowany!';
       } catch (err) {
         console.log('Error');
         this.message = 'Coś poszło nie tak, spróbuj ponownie!';
@@ -125,29 +131,92 @@ export default {
 }
 .container {
   padding: 10px;
+  width: 100%;
+}
+.column {
+  display: flex;
+  flex-direction: column;
 }
 .file-listing {
-  width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin: 5px 0;
   background-color: #dbdddf;
-  padding: 7px 10px;
+  padding: 12px 10px;
   border-radius: 4px;
+}
+.message {
+  font-weight: bold;
+  font-size: 18px;
+  color: #c6c6c6;
 }
 .icons {
   display: flex;
 }
-
 .svg-icon {
   width: 24px;
   margin-left: 6px;
 }
-
 .link {
   color: #0b1223;
   text-decoration: none;
   cursor: pointer;
+}
+.btn {
+  display: inline-block;
+  width: 200px;
+  text-align: center;
+  border-radius: 8px;
+  line-height: 30px;
+  padding: 3px;
+  cursor: pointer;
+  margin-right: 15px;
+  padding: 4px 15px;
+  box-shadow: 0 2px 4px #0b12232a;
+  background: #0b1223;
+  border: 1px solid #c6c6c6;
+  color: #dbdddf;
+  text-decoration: none;
+  transition: background 0.3s 0.2s ease-in-out,
+    color 0.3s 0.1s ease-in-out, box-shadow 0.3s 0.1s ease-in-out;
+}
+
+.btn:hover {
+  background: #dbdddf;
+  box-shadow: 3px 4px 8px #0b12232a;
+  color: #0b1223;
+}
+.file-input {
+  margin-top: 15px;
+  color: transparent;
+  outline: none;
+  width: 200px;
+}
+.file-input::-webkit-file-upload-button {
+  visibility: hidden;
+}
+.file-input::before {
+  content: 'Wybierz plik...';
+  display: inline-block;
+  background: #0b1223;
+  line-height: 30px;
+  border: 1px solid #c6c6c6;
+  color: #dbdddf;
+  border-radius: 8px;
+  padding: 5px 8px;
+  outline: none;
+  text-align: center;
+  white-space: nowrap;
+  user-select: none;
+  cursor: pointer;
+}
+.file-input:hover::before {
+  background: #dbdddf;
+  box-shadow: 3px 4px 8px #0b12232a;
+  color: #0b1223;
+}
+.file-input:active {
+  outline: 0;
 }
 </style>
